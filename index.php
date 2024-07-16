fb9763bf41fadbe9bbb130b3d001e572a372192c
dev-arslanamjad <arslanamjad4444@gmail.com>
2024-07-16 14:01:41 +0500

    added para

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
             <div class="table-wrapper">
                 <div class="table-title">
                     <div class="bg-light p-2 m-2">
-                        <h3 class="text-dark text-center">{button}Students Data Managaement</h3>
+                        <h3 class="text-dark text-center">{paragraph}Students Data Managaement</h3>
                     </div>
                     <div class="row">
                         <div class="col-sm-6">
